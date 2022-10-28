00ba526c42fb5037c988b1cc38664a80adb069e1
nazzarkevich <nazzik.3@gmail.com>
2022-10-28 09:34:57 +0100

    fixed svg loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,17 +30,8 @@ module.exports = {
                 use:  ['style-loader', 'css-loader', 'sass-loader']
             },
             {
-                test:   /\.svg$/,
-                loader: 'svg-sprite-loader'
-            },
-            {
-                test:    /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader:  'eslint-loader',
-                options: {
-                    emitWarning: true,
-                    configFile:  path.join(__dirname, './.eslintrc.json')
-                }
+                test: /\.svg$/,
+                use: ['@svgr/webpack']
             }
         ]
     }
